70e8c96cfe7bdf9ec9c0922e95af958e4f0b34a2
Jihye Shin <jinontop@gmail.com>
2020-09-16 19:39:31 +0200

    Initial solution setup

---
+++ Pyramid/Pyramid.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Pyramid.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
