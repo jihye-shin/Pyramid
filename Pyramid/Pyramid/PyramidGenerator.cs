8193715b38a44f49985e34eb344dfb96094d5553
Jihye Shin <jinontop@gmail.com>
2020-09-20 10:59:05 +0200

    BuildPyramidsWithValues()

--- Pyramid/Pyramid/PyramidGenerator.cs
+++ Pyramid/Pyramid/PyramidGenerator.cs
@@ -13,11 +13,35 @@ namespace Pyramid
             var p = new PyramidGenerator();
             p.GetNewPyramid("1\n8 9\n1 5 9\n4 5 2 3");
         }
-        public void GetNewPyramid(string inputString)
+        public Pyramid GetNewPyramid(string inputString)
         {
             var valuesInLines = StringToList(inputString);
             PrintList(valuesInLines);
-            //return new Pyramid();
+            var root = BuildPyramids(valuesInLines);
+            return root;
+        }
+
+        private Pyramid BuildPyramids(List<string[]> valuesInLines)
+        {
+            var pyramidsWithValues = BuildPyramidsWithValues(valuesInLines);
+            ConnectChildrens();
+            return root;
+        }
+
+        private List<List<Pyramid>> BuildPyramidsWithValues(List<string[]> valuesInLines)
+        {
+            var pyramidsWithValues = new List<List<Pyramid>>();
+            foreach (var line in valuesInLines)
+            {
+                var pyramids = new List<Pyramid>();
+                foreach (var value in line)
+                {
+                    var p = new Pyramid(Int32.Parse(value), null);
+                    pyramids.Add(p);
+                }
+            }
+
+            return pyramidsWithValues;
         }
 
         private List<string[]> StringToList(string inputString)
